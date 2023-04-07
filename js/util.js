cd232d389fc6ab5097e706d4cb76ca98a7838d54
Никита Новиков <tanya270384@mail.ru>
2023-04-07 12:36:38 +0300

    12.12. Перламутровые пуговицы

--- js/util.js
+++ js/util.js
@@ -1,16 +1,23 @@
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
 };
 
-export {getRandomInteger};
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
 
-const getRandomArrayElement = (elements) =>
-  elements[getRandomInteger(0, elements.length - 1)];
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
 
-export {getRandomArrayElement};
+export {shuffleArray, debounce};
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
